9271b4aa89da33130cf115d679347e76d36ea7b9
Piotr Michna <poczta@piotrmichna.pl>
2021-03-06 12:17:42 +0100

    definicja modelu instytucji

--- odr/charity/models.py
+++ odr/charity/models.py
@@ -12,3 +12,13 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=128, null=False, verbose_name='Kategoria')
+    description = models.TextField(max_length=512, null=True, verbose_name='Opis')
+    type = models.SmallIntegerField(choices=TYPE_INSTITUT, default=0, verbose_name='Typ')
+    categories = models.ManyToManyField(Category, verbose_name='Kagegoria')
+
+    def __str__(self):
+        return self.name

d44a78561727160cbb21622ce61f9548be6c1758
Piotr Michna <poczta@piotrmichna.pl>
2021-03-06 12:17:20 +0100

    definicja listy typów instytucji

--- odr/charity/models.py
+++ odr/charity/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+TYPE_INSTITUT = [
+    (0, "fundacja"),
+    (1, "organizacja pozarządowa"),
+    (2, "zbiórka lokalna"),
+]
+
 
 class Category(models.Model):
     name = models.CharField(max_length=128, null=False, verbose_name='Kategoria')

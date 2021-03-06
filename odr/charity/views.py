048d478ae9fae136dc5f2094efd99104b8a5c48b
Piotr Michna <poczta@piotrmichna.pl>
2021-03-06 11:04:15 +0100

    utworzenie widoku strony głównej

--- odr/charity/views.py
+++ odr/charity/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class LandingPageView(View):
+    """STRONA GŁÓWNA - KRÓTKI OPIS PRZEZNACZENIA"""
+
+    def get(self, request):
+        return render(request, 'landing_page.html')

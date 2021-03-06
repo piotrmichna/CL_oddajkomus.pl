cb98e19bb6242884ec6c79590a2f1d3b679f1d5a
Piotr Michna <poczta@piotrmichna.pl>
2021-03-06 13:50:07 +0100

    definicje przykładowych instytucji

--- odr/charity/management/commands_data/charity_data.py
+++ odr/charity/management/commands_data/charity_data.py
@@ -9,3 +9,30 @@ CATEGORY_DATA = [
     'Inne'
 ]
 
+
+INSTITUTION_DATA = [
+    # FUNDACJE
+    ['Instytut Moralnie Zaniedbanych Dzieci', '''Polska instytucja opieki społecznej założona 1 października 1830 
+     w Warszawie. Była pierwszą placówką wychowawczą w Warszawie.''', 0, (1, 2, 3, 4, 5, 6)],
+    ['Fundacja Homo Homini', '''fundacja dobroczynna założona w 1991 roku, w Krakowie, z inicjatywy 
+     Kazimierza Helizanowicza i Marka Rembisia, jako odpowiedź na społeczne 
+      wyzwania związane z kryzysem rodziny.''', 0, (1, 2, 3, 4, 5, 6, 7)],
+    ['Fundacja Kwiaty Polskie', '''fundacja zajmująca się działaniami na rzecz osób niepełnosprawnych i 
+    pomocą społeczną - rodzinom i osobom w trudnej sytuacji życiowej z tytułu choroby dzieci lub 
+     niewydolności socjalnej. ''', 0, (1, 2, 3, 4, 5, 6, 7)],
+    ['Narodowe Towarzystwo Autystyczne', ''' brytyjska organizacja charytatywna działająca na rzecz osób z 
+    zaburzeniami ze spektrum autyzmu. ''', 0, (1, 2, 4, 6)],
+    ['Stowarzyszenie Immunoprotect', '''organizacja pozarządowa działająca na rzecz osób z 
+    niedoborami odporności, ze szczególnym uwzględnieniem pierwotnych
+    (wrodzonych) niedoborów odporności.''', 0, (1, 2, 3, 4)],
+    ['Skrzynka świętego Mikołaja', '''funduszy dobroczynnych działających od 1588 roku przy 
+    krakowskim kościele św. Barbary w ramach Arcybractwa Miłosierdzia. Celem skrzynki świętego Mikołaja 
+    było udzielanie pomocy w formie posagu ubogim pannom wychodzącym za 
+    mąż bądź wstępującym do klasztoru.''', 0, (1, 2, 3, 4)],
+    # ORGANIZACJE POZARZĄDOWE
+    ['Polska Akcja Humanitarna', '''organizacja o statusie organizacji pożytku publicznego. Założona przez 
+    amerykańskiego finansistę, George’a Sorosa oraz grupę działaczy polskiej opozycji lat 80.''', 1, (1, 2, 4)],
+
+    ['Herkules - Wsparcie bezdomnych', 'Zbiórka lokalna dla dzieci z Dom Dziecka Deszczółka w Krakowie', 2, (1, 2, 3)],
+    ['Łapu-capu - Zbiórka', 'Zbiórka lokalna dla osób bezdomnych Krakowie', 2, (1, 2, 3, 5, 6, 7)],
+]

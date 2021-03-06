206c1339fd0eb541379abc0b3efeee06b162dbf7
Piotr Michna <poczta@piotrmichna.pl>
2021-03-06 13:50:58 +0100

    definicje przykladowych darowizn

--- odr/charity/management/commands_data/charity_data.py
+++ odr/charity/management/commands_data/charity_data.py
@@ -9,7 +9,6 @@ CATEGORY_DATA = [
     'Inne'
 ]
 
-
 INSTITUTION_DATA = [
     # FUNDACJE
     ['Instytut Moralnie Zaniedbanych Dzieci', '''Polska instytucja opieki społecznej założona 1 października 1830 
@@ -35,4 +34,40 @@ INSTITUTION_DATA = [
 
     ['Herkules - Wsparcie bezdomnych', 'Zbiórka lokalna dla dzieci z Dom Dziecka Deszczółka w Krakowie', 2, (1, 2, 3)],
     ['Łapu-capu - Zbiórka', 'Zbiórka lokalna dla osób bezdomnych Krakowie', 2, (1, 2, 3, 5, 6, 7)],
+]
+
+DONATION_DATA = [
+    [12, (1, 2, 3, 4), 1,
+     'Puławska 97', 600300200, 'Warszawa', '00-123',
+     '2020-02-12', '16:00:00', 'Akcja bedzie trwała 2 godziny.'],
+    [31, (1, 2, 3, 5, 7), 2,
+     'Ks. Popiełuszki 43', 601414508, 'Kraków', '30-898',
+     '2021-01-05', '16:00:00', 'Akcja bedzie trwała 2 godziny.'],
+    [11, (1, 2, 3, 5, 7), 3,
+     'Wita Stwosza 73', 601414508, 'Gdańsk', '40-398',
+     '2020-10-05', '16:00:00', 'Akcja bedzie trwała 2 godziny.'],
+    [2, (1, 2, 3, 5, 7), 4,
+     '393 City Road', 2078339666, 'London', 'EC1V1N',
+     '2019-06-15', '16:00:00', 'Bezterminowo.'],
+    [21, (1, 2, 3, 5, 7), 5,
+     'Ossendowskiego 7', 517280858, 'Łódź', '93-228',
+     '2020-06-12', '16:00:00', 'Bezterminowo.'],
+    [7, (1, 4, 5, 7), 6,
+     'Plac Mariacki', 517280858, 'Kraków', '30-898',
+     '2021-01-05', '16:00:00', 'Bezterminowo.'],
+
+    [1, (1, 4, 5, 7), 7,
+     'Sapieżyńska 10', 225360200, 'Warszawa', '00-215',
+     '2021-03-15', '16:00:00', 'Bezterminowo.'],
+
+    [23, (1, 2, 3, 5, 7), 8,
+     'Plac Mariacki', 225360200, 'Kraków', '00-215',
+     '2021-03-05', '16:00:00', 'Bezterminowo.'],
+
+    [17, (1, 2, 3, 5, 7), 9,
+     'Plac Mariacki', 225360200, 'Kraków', '00-215',
+     '2021-02-05', '16:00:00', 'Bezterminowo.'],
+    [12, (1, 2, 3, 5, 7), 9,
+     'Plac Mariacki', 225360200, 'Kraków', '00-215',
+     '2021-01-05', '16:00:00', 'Bezterminowo.'],
 ]

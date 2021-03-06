d8fcaac7f2bbdd5766d5e2246a94309dbc344830
Piotr Michna <poczta@piotrmichna.pl>
2021-03-06 13:55:38 +0100

    definicja funkcji dodajacej instytucji

--- odr/charity/management/commands/insert_charity_data.py
+++ odr/charity/management/commands/insert_charity_data.py
@@ -1,10 +1,15 @@
-from django.core.management import BaseCommand
-
-from charity.management.commands_data.charity_data import CATEGORY_DATA
-from charity.models import Category
+from charity.management.commands_data.charity_data import CATEGORY_DATA, INSTITUTION_DATA
+from charity.models import Category, Institution
 
 
 def insert_category():
     for name in CATEGORY_DATA:
         Category.objects.create(name=name)
 
+
+def insert_institutions():
+    for name, description, type, categories in INSTITUTION_DATA:
+        inst = Institution.objects.create(name=name, description=description, type=type)
+        for catid in categories:
+            inst.categories.add(Category.objects.get(id=catid))
+        inst.save()
